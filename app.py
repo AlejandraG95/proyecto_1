be985860563757378b006070941b4fecd57d2375
Alejandra Gonzalez Parra <107818740+AlejandraG95@users.noreply.github.com>
2024-04-11 12:21:51 -0500

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from controllers.actualizarps_w import ActualizarpsWindow
-
-if __name__== '__main__':
-     app=QApplication()
-     window= ActualizarpsWindow()
-     window.show()
-     app.exec()
